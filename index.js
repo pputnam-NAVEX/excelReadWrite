2f6d423cc53d7f27939e8979bdc3d1657e2bf703
pputnam-NAVEX <philip.putnam@navexglobal.com>
2021-08-13 19:48:26 -0700

    successfully validate if column is empty other than top header

--- index.js
+++ index.js
@@ -56,38 +56,56 @@ ipcMain.on("toMain", (event, args) => {
                 // console.log("actualColumnCount:");
                 // console.log(workbook.worksheets[0].actualColumnCount);
                 
-                let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                // let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
                 let DPData = ["true","false","yes","no","y","n",true,false];
                 let rowOne = workbook.worksheets[0].getRow('1');
               
                 // better way to iterate through row/columns
                 rowOne.eachCell(function(cell, colNumber) { 
-                    var validData = false;
-
+                    
                     if (cell.value == "Data Privacy") {
-                        console.log("cell.column************")
-                        console.log(cell.column);
-                        console.log('colNumber*************')
-                        console.log(colNumber);
-                        console.log('cell**************')
-                        console.log(cell);
-                        console.log('cell address***********')
-                        console.log(cell.address);
-                        console.log('cell ***********')
+                        
                         // regex searches for the column letter(s) in the cell address
                         console.log(cell.address.match(/^[A-Z]*[^0-9]/g))
-                        // for (value in DPData) {
-                        //     if (cell.value == DPData[value]) {
-                        //         validData = true;
-                        //         break
-                        //     }
+                        let cellColumn = cell.address.match(/^[A-Z]*[^0-9]/g)[0];
+                        let theColumn = workbook.worksheets[0].getColumn(cellColumn);
+                        
+                        // check if column has data in it at all
+                        let isColumnEmpty = true;
+                        for (row = 2; row <= workbook.worksheets[0].rowCount; row++) {
+                            if (workbook.worksheets[0].getCell(cellColumn + row).value != null) {
+                                isColumnEmpty = false;
+                                row = workbook.worksheets[0].rowCount + 1;
+                            };
+                        }
+
+                        // if column isn't empty perform data validation
+                        if (!isColumnEmpty) {
+                            theColumn.eachCell({ includeEmpty: true }, function(cell, rowNumber) {
+                                // skip column header data, e.g. "Data Privacy" or "Tier Name"
+                                if (rowNumber > 1) {
+                                    let validData = false;
+                                    if (cell.value == null || cell.value == undefined || cell.value == '') {
+                                        console.log("empty cell in Data Privacy column " + cell.address);
+                                    } else {
+                                        for (value in DPData) {
+                                            if (cell.value.toLowerCase() == DPData[value]) {
+                                                validData = true;
+                                                break
+                                            }
+                    
+                                            if (value == DPData.length-1 && !validData) {
+                                                console.log("invalid data in cell " + cell.address);
+                                            }
+                                        }
+                                    }
     
-                        //     if (validData) {
-                        //         break
-                        //     } else if (value == DPData.length-1 && !ValidData) {
-                        //         console.log("invalid data on row" + data);
-                        //     }
-                        // }
+                                }
+                            });
+                        } else {
+                            console.log('Column is empty')
+                        }
+
                     }
 
                 });
